4af43d0b5e8db8cdc7b6d1bd5897d6ce061924a2
snehak2299 <skanase2299@gmail.com>
2021-08-09 17:34:08 +0530

    [sneha]:genrate random no to select option no play , ladder ,snake

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,11 +1,35 @@
 
 public class SnakeLadder{
 	public static void main (String args[]){
-	  int START_POSITION = 0;
+	  int position= 0;
 	  int min = 1;
 	  int max = 6;
 	  int randomDiesNumber = (int)(Math.random() * ((max - min) + 1)) + min;
 	  System.out.println("random value genrated for dies is:" + randomDiesNumber);
+	  int lowerbound=1;
+    int upperbound=3;
+    int option = (int)(Math.random() * ((upperbound - lowerbound) + 1)) + lowerbound;
+    System.out.println(option);
+    switch(option){
+  	  case 1:{
+       System.out.println("no play");
+       break;
+	  }
+      case 2:{
+       System.out.println("Ladder");
+       position = position+randomDiesNumber;
+       break;
+	  }
+      case 3:{
+       System.out.println("snake");
+       position=position-randomDiesNumber;
+       break;
+	  }
+
+    }
+  
+   System.out.println(position);
+    
       
 	}
 }
