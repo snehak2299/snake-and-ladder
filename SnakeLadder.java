cbb89a99d2c5917b4c0d5ba62db8863c9b56cb4d
snehak2299 <skanase2299@gmail.com>
2021-08-09 16:41:18 +0530

    [sneha]:random number for dies is genrated btwn 1 to 6

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,5 +1,11 @@
+
 public class SnakeLadder{
 	public static void main (String args[]){
 	  int START_POSITION = 0;
+	  int min = 1;
+	  int max = 6;
+	  int randomDiesNumber = (int)(Math.random() * ((max - min) + 1)) + min;
+	  System.out.println("random value genrated for dies is:" + randomDiesNumber);
+      
 	}
 }
